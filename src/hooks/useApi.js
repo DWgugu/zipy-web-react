239399ee33537e531b30e614b4c11b80e1d64604
anderson <guguwagner@hotmail.com>
2023-01-27 09:59:59 -0300

    Alterado chamada da API do .env para import.meta

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API
+    baseURL: import.meta.env.REACT_APP_API
 });
 
 const useApi = () => ({
@@ -11,4 +11,4 @@ const useApi = () => ({
     } 
 })
 
-export default useApi();
+export default useApi;
